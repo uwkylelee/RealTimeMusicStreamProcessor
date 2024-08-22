003e0905ff0639520bf7e3f0d8f3abdc4a155049
Kyle Lee <js66lee@uwaterloo.ca>
2024-08-22 16:48:21 -0400

    docs: edit .gitignore

--- kafka/producer/src/producer.py
+++ kafka/producer/src/producer.py
@@ -16,18 +16,12 @@ class Producer:
             value_serializer=lambda v: json.dumps(v).encode('utf-8')
         )
         self.fake = Faker('ko_KR')
-        self.hashtags = ['#데이터', '#AI', '#빅데이터', '#Kafka', '#Python', '#프로젝트',
-                         '#개발자', '#기술']
 
-    def generate_social_media_data(self) -> Dict[str, Any]:
+    def generate_base_music_streaming_data(self) -> Dict[str, Any]:
         data: Dict[str, Any] = {
-            'user_id': self.fake.user_name(),
-            'timestamp': datetime.utcnow().isoformat(),
-            'message': self.fake.sentence(nb_words=10),
-            'likes': random.randint(0, 1000),
-            'comments': random.randint(0, 500),
-            'shares': random.randint(0, 200),
-            'hashtags': random.sample(self.hashtags, random.randint(1, 3))
+            "user_id": random.randint(1, 1000),
+            "track_id": random.randint(1, 50000),
+            "timestamp": self.fake.date_time_this_month().isoformat()
         }
         return data
 
@@ -38,19 +32,19 @@ class Producer:
     def run(self, interval: float = 1.0) -> None:
         try:
             while True:
-                data = self.generate_social_media_data()
-                print(f"생성된 소셜 미디어 데이터: {data}")
+                data = self.generate_base_music_streaming_data()
+                print(f"Generate Music Streaming Data: {data}")
                 self.send_data(data)
                 time.sleep(interval)
         except KeyboardInterrupt:
-            print("데이터 생성기를 종료합니다.")
+            print("Stop generating music streaming data.")
         finally:
             self.producer.close()
 
 
 if __name__ == '__main__':
     kafka_servers = ['kafka:9092']  # Kafka 브로커 주소
-    topic = 'social_media_data'  # 전송할 토픽 이름
+    topic = 'music_streaming_data'  # 전송할 토픽 이름
 
     data_generator = Producer(kafka_servers, topic)
     data_generator.run(interval=1.0)  # 1초마다 데이터 생성
