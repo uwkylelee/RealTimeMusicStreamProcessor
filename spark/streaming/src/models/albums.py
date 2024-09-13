1c35cf9a0728dc501dee5d6ec4853e627c9ef64c
Kyle Lee <js66lee@uwaterloo.ca>
2024-09-12 20:34:43 -0400

    chore: remove unused files (due to the modification in filetree

--- spark/streaming/src/models/albums.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, Text
-from models.base import Base
-
-
-class Albums(Base):
-    __tablename__ = 'albums'
-
-    album_id = Column(Integer, primary_key=True, autoincrement=True)
-    album_name = Column(Text, nullable=False, unique=True)
--- spark/streaming/src/models/artists.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, Text
-from models.base import Base
-
-
-class Artists(Base):
-    __tablename__ = 'artists'
-
-    artist_id = Column(Integer, primary_key=True, autoincrement=True)
-    artist_name = Column(Text, nullable=False, unique=True)
